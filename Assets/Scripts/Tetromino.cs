c0e45b1bc136778baf3997694c20375b5e34378d
Can Akdag <canakdag06@gmail.com>
2024-11-03 11:37:51 +0300

    created spawning methods

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public enum Tetromino
@@ -16,4 +17,10 @@ public struct TetrominoData
 {
     public Tetromino tetromino;
     public Tile tile;
+    public Vector2Int[] cells { get; private set; }
+
+    public void Initialize()
+    {
+        this.cells = Data.Cells[this.tetromino];
+    }
 }
