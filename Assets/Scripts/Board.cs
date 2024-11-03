e576e14906457cd44713424748d59b40e408eebd
Can Akdag <canakdag06@gmail.com>
2024-11-03 18:30:01 +0300

    created Clear( )

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -54,6 +54,15 @@ public class Board : MonoBehaviour
         }
     }
 
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
     public bool IsValidPosition(Piece piece, Vector3Int position)
     {
         for (int i = 0; i < piece.cells.Length; i++)
