6d85a6446db2c85b1b720d5a078245c94f164f85
Can Akdag <canakdag06@gmail.com>
2025-05-28 22:22:17 +0300

    fixed DOTween destroy issue

--- Assets/Scripts/ButtonScaleEffect.cs
+++ Assets/Scripts/ButtonScaleEffect.cs
@@ -4,13 +4,15 @@ using UnityEngine.EventSystems;
 
 public class ButtonScaleEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        transform.DOScale(1.1f, 0.2f).SetEase(Ease.OutBack);
+        transform.DOScale(1.1f, 0.2f).SetEase(Ease.OutBack).SetLink(gameObject);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        transform.DOScale(1f, 0.2f).SetEase(Ease.OutBack);
+        transform.DOScale(1f, 0.2f).SetEase(Ease.OutBack).SetLink(gameObject);
     }
 }
